5ad2db795b6686122cab1e584a95749fbf2fae05
Alejandro Scheiffer <ascheiffer@gmail.com>
2022-12-21 18:55:56 -0300

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+const Navbar = () => {
+    return (
+
+        <nav>
+            <h1>Boom Piscinas</h1>
+
+            <div>
+                <button>Piscinas</button>
+                <button>Accesorios</button>
+                <button>Reposeras</button>
+
+
+            </div>
+
+
+        </nav>
+
+
+
+
+
+
+    )
+
+
+}
+
+export default Navbar
